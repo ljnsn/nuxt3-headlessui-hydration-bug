767163d402ca4bdf4b7d7862839c0fae12995412
ljnsn <lbj@goja.me>
2022-05-22 14:23:43 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtWelcome />
+    <Dialog :open="isOpen" @close="toggleModal"> </Dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Dialog } from "@headlessui/vue";
+
+const isOpen = ref(false);
+
+const toggleModal = () => {
+  isOpen.value = isOpen.value ? false : true;
+};
+</script>
+
